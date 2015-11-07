fb651ec3a237d7eb83d27c63e3443d0524f2fa5d
Ryan Newton <rnewton@hearsaycorp.com>
2015-11-07 12:31:05 -0800

    Add gesture recognizer to look voting cell

--- ClothesConsensus/LooksListing/LookVotingTableViewCell.swift
+++ ClothesConsensus/LooksListing/LookVotingTableViewCell.swift
@@ -13,18 +13,20 @@ protocol LookVotingTableViewCellDelegate {
 }
 
 
-class LookVotingTableViewCell: UITableViewCell {
+class LookVotingTableViewCell: UITableViewCell, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var lookImageView: UIImageView!
+    @IBOutlet weak var votingSlider: UISlider!
     
     var delegate: LookVotingTableViewCellDelegate?
     var look: Look?
-    
+    var startingPoint: CGPoint = CGPoint()
 
-    @IBOutlet weak var votingSlider: UISlider!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setupSlider()
+        setupGestureRecognizer()
         setupStyles()
     }
     
@@ -52,11 +54,42 @@ class LookVotingTableViewCell: UITableViewCell {
         votingSlider.maximumTrackTintColor = grayBarColor
     }
     
+    func setupGestureRecognizer() {
+        var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "onPan:")
+        lookImageView.userInteractionEnabled = true
+        lookImageView.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    
     @IBAction func onVoteSliderSlid(sender: UISlider) {
         let voteValue = sender.value
         self.delegate?.onVoteSliderSlid(self, voteValue: voteValue)
     }
+    
+    @IBAction func onPan(sender: UIPanGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.Began {
+            startingPoint = sender.locationInView(self)
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            var currentPoint = sender.locationInView(self)
+            var difference = calculateVotingValueFromPoints(startingPoint, endingPoint: currentPoint)
+            setSliderPositionBasedOnVotingValue(difference)
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            var currentPoint = sender.locationInView(self)
+            var difference = calculateVotingValueFromPoints(startingPoint, endingPoint: currentPoint)
+        }
+    }
 
+    func calculateVotingValueFromPoints(startingPoint: CGPoint, endingPoint: CGPoint) -> CGFloat {
+        var horizontalDifference = endingPoint.x - startingPoint.x
+        var halfViewWidth = self.bounds.size.width / 2
+        return horizontalDifference / halfViewWidth
+    }
+    
+    func setSliderPositionBasedOnVotingValue(voteValue: CGFloat) {
+        votingSlider.value = Float(CGFloat(0.5) + voteValue)
+        self.delegate?.onVoteSliderSlid(self, voteValue: Float(voteValue))
+    }
+    
     @IBAction func sliderSliding(sender: UISlider) {
         if (sender.value < 0.5) {
             // If it's negative
@@ -65,8 +98,7 @@ class LookVotingTableViewCell: UITableViewCell {
             // If it's positive
             let trueValue = (sender.value - 0.5) * 2
         }
-    }
-    
+    }    
     
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
